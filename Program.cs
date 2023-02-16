1f407da2f08375b6396faf529a5265cb15f52975
Denfit9 <79207007+Denfit9@users.noreply.github.com>
2023-02-16 19:58:57 +0300

    Final version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
@@ -15,19 +16,45 @@ void ClearCurrentConsoleLine()
 IPAddress localAddress = IPAddress.Parse("127.0.0.1");
 Console.Write("Enter your name: ");
 string? username = Console.ReadLine();
-Console.Write("Enter a numebr of port to listen: ");
-if (!int.TryParse(Console.ReadLine(), out var localPort)) return;
+while (string.IsNullOrEmpty(username))
+{
+    Console.WriteLine("Empty names are not allowed\nEnter your name:  ");
+    username = Console.ReadLine();
+}
+var localPort=0;
+Console.Write("Enter a number of port to listen: ");
+while (!int.TryParse(Console.ReadLine(), out  localPort))
+{
+    Console.Write("Such port is not allowed\nEnter a number of port to listen: ");
+    //int.TryParse(Console.ReadLine(), out localPort);
+}
+    
 Console.Write("Enter a number of port to send: ");
-if (!int.TryParse(Console.ReadLine(), out var remotePort)) return;
-Console.WriteLine();
+var remotePort = 0;
+while (!int.TryParse(Console.ReadLine(), out remotePort))
+{
+    Console.Write("Such port is not allowed\nEnter a number of port to send: ");
+    //int.TryParse(Console.ReadLine(), out remotePort);
+}
+Console.Clear();
+
 string path = localPort + "," + remotePort + "=" + username + ".txt";
 using (StreamWriter fileStream = File.Exists(path) ? File.AppendText(path) : File.CreateText(path))
 {
-   
+
 }
 using (StreamReader reader = new StreamReader(path))
 {
+
     string text = await reader.ReadToEndAsync();
+    if (string.IsNullOrEmpty(text))
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Previous session messages: \n");
+    }
     Console.WriteLine(text);
 }
 
@@ -37,12 +64,16 @@ await SendMessageAsync();
 async Task SendMessageAsync()
 {
     using Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    byte[] greet = Encoding.UTF8.GetBytes("Your interlocutor just connected");
+    await sender.SendToAsync(greet, new IPEndPoint(localAddress, remotePort));
     Console.WriteLine("Enter a message and then press enter button to send it or type quit to leave ");
     while (true)
     {
         var message = Console.ReadLine();
         if(message == "quit")
         {
+            byte[] left = Encoding.UTF8.GetBytes("Your interlocutor left");
+            await sender.SendToAsync(left, new IPEndPoint(localAddress, remotePort));
             return;
         }
         var messageCopy = message;
@@ -50,6 +81,7 @@ async Task SendMessageAsync()
         message = $"{username}: {message}";
         byte[] data = Encoding.UTF8.GetBytes(message);
         await sender.SendToAsync(data, new IPEndPoint(localAddress, remotePort));
+
         Console.SetCursorPosition(0, Console.CursorTop - 1);
         ClearCurrentConsoleLine();
         Console.WriteLine("You: " + messageCopy);
@@ -57,6 +89,7 @@ async Task SendMessageAsync()
         {
            writer.WriteLine("You: " + messageCopy);
         }
+
     }
 }
 
@@ -72,7 +105,11 @@ async Task ReceiveMessageAsync()
         Console.WriteLine(message);
         using (StreamWriter writer = new StreamWriter(path, true, System.Text.Encoding.Default))
         {
-            writer.WriteLine(message);
+            if(message== "Your interlocutor left" || message == "Your interlocutor just connected") { }
+            else
+            {
+                writer.WriteLine(message);
+            }
         }
     }
 }
